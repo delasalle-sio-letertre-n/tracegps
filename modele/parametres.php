a235c0adfde1ce65ba693c5d0329fa49aa8cea97
delasalle-sio-letertre-n <delasalle.sio.letertre.n@gmail.com>
2022-10-19 16:43:48 +0200

    Nicolas > Décommenté DAO.test.php et configuration fichier parametres.php

--- modele/parametres.php
+++ modele/parametres.php
@@ -8,8 +8,8 @@ global $PARAM_HOTE, $PARAM_PORT, $PARAM_BDD, $PARAM_USER, $PARAM_PWD;
 $PARAM_HOTE = "localhost";		// si le sgbd est sur la même machine que le serveur php
 $PARAM_PORT = "3306";			// le port utilisé par le serveur MySql
 $PARAM_BDD = "tracegps";		// nom de la base de données
-$PARAM_USER = "tracegps";		// nom de l'utilisateur
-$PARAM_PWD = "spgecart";		// son mot de passe
+$PARAM_USER = "root";		// nom de l'utilisateur
+$PARAM_PWD = "";		// son mot de passe
 
 // Autres paramètres -----------------------------------------------------------------------------------------
 
